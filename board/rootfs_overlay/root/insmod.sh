32d4f3c41d1c7388d5b3ea0d02b6a77902e9740e
衣川亮太 <kinukawa@tombo-works.com>
2022-01-22 20:28:17 +0900

    feat: modify insmod.sh

--- board/rootfs_overlay/root/insmod.sh
+++ board/rootfs_overlay/root/insmod.sh
@@ -2,12 +2,33 @@
 
 KERNEL_VER=$(uname -r)
 
+# rmmod
+
+## wifi
 rmmod "/lib/modules/$KERNEL_VER/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko"
 rmmod "/lib/modules/$KERNEL_VER/kernel/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko"
 rmmod "/lib/modules/$KERNEL_VER/kernel/net/wireless/cfg80211.ko"
+
+## bluetooth
+rmmod "/lib/modules/$KERNEL_VER/kernel/drivers/bluetooth/hci_uart.ko"
+rmmod "/lib/modules/$KERNEL_VER/kernel/drivers/bluetooth/btbcm.ko"
+rmmod "/lib/modules/$KERNEL_VER/kernel/net/bluetooth/bluetooth.ko"
+
+## rfkill
 rmmod "/lib/modules/$KERNEL_VER/kernel/net/rfkill/rfkill.ko"
 
+
+# insmod
+
+## rfkill
 insmod "/lib/modules/$KERNEL_VER/kernel/net/rfkill/rfkill.ko"
+
+## wifi
 insmod "/lib/modules/$KERNEL_VER/kernel/net/wireless/cfg80211.ko"
 insmod "/lib/modules/$KERNEL_VER/kernel/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko"
 insmod "/lib/modules/$KERNEL_VER/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko"
+
+## bluetooth
+insmod "/lib/modules/$KERNEL_VER/kernel/net/bluetooth/bluetooth.ko"
+insmod "/lib/modules/$KERNEL_VER/kernel/drivers/bluetooth/btbcm.ko"
+insmod "/lib/modules/$KERNEL_VER/kernel/drivers/bluetooth/hci_uart.ko"
